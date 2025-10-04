db8d8398c65ba1e68f8997a9d7259d876343aab9
VidOne <debsharmasharma6@gmail.com>
2025-10-04 12:00:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,39 @@
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAs82Or7bJPuBE3A_QmRAz18CkRb9iKRZE",
+    authDomain: "bahari-52fdb.firebaseapp.com",
+    databaseURL: "https://bahari-52fdb-default-rtdb.firebaseio.com",
+    projectId: "bahari-52fdb",
+    storageBucket: "bahari-52fdb.appspot.com",
+    messagingSenderId: "89195698661",
+    appId: "1:89195698661:web:b8fa8277a2103da37b370c",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+
+// --- PWA Caching Logic ---
+
 const CACHE_NAME = 'bahari-cache-v1';
 const urlsToCache = [
   '/',
